38e36b8ca6bc7cf164c67c1266a1b9f102fc0e31
TopiSenpai <tobi@strasserhof.com>
2022-11-28 11:33:10 +0100

    use new plugin data appender interface

--- plugin/src/main/java/com/github/topisenpai/lavasrc/plugin/LavaSrcPluginDataAppender.java
+++ plugin/src/main/java/com/github/topisenpai/lavasrc/plugin/LavaSrcPluginDataAppender.java
@@ -12,18 +12,43 @@ import com.github.topisenpai.lavasrc.yandexmusic.YandexMusicAudioPlaylist;
 import com.github.topisenpai.lavasrc.yandexmusic.YandexMusicAudioTrack;
 import com.sedmelluq.discord.lavaplayer.track.AudioPlaylist;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
-import dev.arbjerg.lavalink.api.AudioPlaylistJsonAppender;
-import dev.arbjerg.lavalink.api.AudioTrackJsonAppender;
+import dev.arbjerg.lavalink.api.JsonPluginDataAppender;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Component
-public class LavaSrcJsonAppender implements AudioTrackJsonAppender, AudioPlaylistJsonAppender {
+public class LavaSrcPluginDataAppender implements JsonPluginDataAppender {
 
 	@Override
-	public Map<String, JsonNode> appendFields(AudioPlaylist playlist) {
+	public Map<String, JsonNode> addTrackPluginData(AudioTrack audioTrack) {
+		var map = new HashMap<String, JsonNode>();
+
+		if (audioTrack instanceof SpotifyAudioTrack) {
+			var spotifyAudioTrack = (SpotifyAudioTrack) audioTrack;
+			map.put("artworkURL", new TextNode(spotifyAudioTrack.getArtworkURL()));
+			map.put("isrc", new TextNode(spotifyAudioTrack.getISRC()));
+
+		} else if (audioTrack instanceof AppleMusicAudioTrack) {
+			var appleMusicAudioTrack = (AppleMusicAudioTrack) audioTrack;
+			map.put("artworkURL", new TextNode(appleMusicAudioTrack.getArtworkURL()));
+			map.put("isrc", new TextNode(appleMusicAudioTrack.getISRC()));
+
+		} else if (audioTrack instanceof DeezerAudioTrack) {
+			var deezerAudioTrack = (DeezerAudioTrack) audioTrack;
+			map.put("artworkURL", new TextNode(deezerAudioTrack.getArtworkURL()));
+			map.put("isrc", new TextNode(deezerAudioTrack.getISRC()));
+		} else if (audioTrack instanceof YandexMusicAudioTrack) {
+			var yandexMusicAudioTrack = (YandexMusicAudioTrack) audioTrack;
+			map.put("artworkURL", new TextNode(yandexMusicAudioTrack.getArtworkURL()));
+		}
+
+		return map;
+	}
+
+	@Override
+	public Map<String, JsonNode> addPlaylistPluginData(AudioPlaylist playlist) {
 		var map = new HashMap<String, JsonNode>();
 
 		if (playlist instanceof SpotifyAudioPlaylist) {
@@ -58,29 +83,4 @@ public class LavaSrcJsonAppender implements AudioTrackJsonAppender, AudioPlaylis
 		return map;
 	}
 
-	@Override
-	public Map<String, JsonNode> appendFields(AudioTrack audioTrack) {
-		var map = new HashMap<String, JsonNode>();
-
-		if (audioTrack instanceof SpotifyAudioTrack) {
-			var spotifyAudioTrack = (SpotifyAudioTrack) audioTrack;
-			map.put("artworkURL", new TextNode(spotifyAudioTrack.getArtworkURL()));
-			map.put("isrc", new TextNode(spotifyAudioTrack.getISRC()));
-
-		} else if (audioTrack instanceof AppleMusicAudioTrack) {
-			var appleMusicAudioTrack = (AppleMusicAudioTrack) audioTrack;
-			map.put("artworkURL", new TextNode(appleMusicAudioTrack.getArtworkURL()));
-			map.put("isrc", new TextNode(appleMusicAudioTrack.getISRC()));
-
-		} else if (audioTrack instanceof DeezerAudioTrack) {
-			var deezerAudioTrack = (DeezerAudioTrack) audioTrack;
-			map.put("artworkURL", new TextNode(deezerAudioTrack.getArtworkURL()));
-			map.put("isrc", new TextNode(deezerAudioTrack.getISRC()));
-		} else if (audioTrack instanceof YandexMusicAudioTrack) {
-			var yandexMusicAudioTrack = (YandexMusicAudioTrack) audioTrack;
-			map.put("artworkURL", new TextNode(yandexMusicAudioTrack.getArtworkURL()));
-		}
-
-		return map;
-	}
 }
