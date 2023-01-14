a4a67009f589f3ebbce63724e8e5ecd089fc2ead
TopiSenpai <tobi@strasserhof.com>
2023-01-15 00:02:23 +0100

    rename artworkURL -> artworkUrl

--- plugin/src/main/java/com/github/topisenpai/lavasrc/plugin/LavaSrcAudioPluginInfoModifier.java
+++ plugin/src/main/java/com/github/topisenpai/lavasrc/plugin/LavaSrcAudioPluginInfoModifier.java
@@ -19,25 +19,25 @@ public class LavaSrcAudioPluginInfoModifier implements AudioPluginInfoModifier {
 			var spotifyPlaylist = (SpotifyAudioPlaylist) playlist;
 			node.set("type", new TextNode(spotifyPlaylist.getType()));
 			node.set("identifier", new TextNode(spotifyPlaylist.getIdentifier()));
-			node.set("artworkURL", new TextNode(spotifyPlaylist.getArtworkURL()));
+			node.set("artworkUrl", new TextNode(spotifyPlaylist.getArtworkURL()));
 			node.set("author", new TextNode(spotifyPlaylist.getAuthor()));
 		} else if (playlist instanceof AppleMusicAudioPlaylist) {
 			var appleMusicPlaylist = (AppleMusicAudioPlaylist) playlist;
 			node.set("type", new TextNode(appleMusicPlaylist.getType()));
 			node.set("identifier", new TextNode(appleMusicPlaylist.getIdentifier()));
-			node.set("artworkURL", new TextNode(appleMusicPlaylist.getArtworkURL()));
+			node.set("artworkUrl", new TextNode(appleMusicPlaylist.getArtworkURL()));
 			node.set("author", new TextNode(appleMusicPlaylist.getAuthor()));
 		} else if (playlist instanceof DeezerAudioPlaylist) {
 			var deezerPlaylist = (DeezerAudioPlaylist) playlist;
 			node.set("type", new TextNode(deezerPlaylist.getType()));
 			node.set("identifier", new TextNode(deezerPlaylist.getIdentifier()));
-			node.set("artworkURL", new TextNode(deezerPlaylist.getArtworkURL()));
+			node.set("artworkUrl", new TextNode(deezerPlaylist.getArtworkURL()));
 			node.set("author", new TextNode(deezerPlaylist.getAuthor()));
 		} else if (playlist instanceof YandexMusicAudioPlaylist) {
 			var yandexMusicPlaylist = (YandexMusicAudioPlaylist) playlist;
 			node.set("type", new TextNode(yandexMusicPlaylist.getType()));
 			node.set("identifier", new TextNode(yandexMusicPlaylist.getIdentifier()));
-			node.set("artworkURL", new TextNode(yandexMusicPlaylist.getArtworkURL()));
+			node.set("artworkUrl", new TextNode(yandexMusicPlaylist.getArtworkURL()));
 			node.set("author", new TextNode(yandexMusicPlaylist.getAuthor()));
 		}
 	}
