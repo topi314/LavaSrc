d81d18f92c5e4f8d86a922a1ac88a0caff054f57
TopiSenpai <git@topi.wtf>
2023-05-25 13:51:59 +0200

    update to latest lavalink

--- plugin/src/main/java/com/github/topisenpai/lavasrc/plugin/LavaSrcAudioPluginInfoModifier.java
+++ plugin/src/main/java/com/github/topisenpai/lavasrc/plugin/LavaSrcAudioPluginInfoModifier.java
@@ -1,44 +1,30 @@
 package com.github.topisenpai.lavasrc.plugin;
 
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.fasterxml.jackson.databind.node.TextNode;
-import com.github.topisenpai.lavasrc.applemusic.AppleMusicAudioPlaylist;
-import com.github.topisenpai.lavasrc.deezer.DeezerAudioPlaylist;
-import com.github.topisenpai.lavasrc.spotify.SpotifyAudioPlaylist;
-import com.github.topisenpai.lavasrc.yandexmusic.YandexMusicAudioPlaylist;
+import com.github.topisenpai.lavasrc.ExtendedAudioPlaylist;
 import com.sedmelluq.discord.lavaplayer.track.AudioPlaylist;
 import dev.arbjerg.lavalink.api.AudioPluginInfoModifier;
+import kotlinx.serialization.json.JsonElementKt;
+import kotlinx.serialization.json.JsonObject;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Component;
+
+import java.util.Map;
 
 @Component
 public class LavaSrcAudioPluginInfoModifier implements AudioPluginInfoModifier {
 
-	@Override
-	public void modifyAudioPlaylistPluginInfo(AudioPlaylist playlist, ObjectNode node) {
-		if (playlist instanceof SpotifyAudioPlaylist) {
-			var spotifyPlaylist = (SpotifyAudioPlaylist) playlist;
-			node.set("type", new TextNode(spotifyPlaylist.getType()));
-			node.set("identifier", new TextNode(spotifyPlaylist.getIdentifier()));
-			node.set("artworkUrl", new TextNode(spotifyPlaylist.getArtworkURL()));
-			node.set("author", new TextNode(spotifyPlaylist.getAuthor()));
-		} else if (playlist instanceof AppleMusicAudioPlaylist) {
-			var appleMusicPlaylist = (AppleMusicAudioPlaylist) playlist;
-			node.set("type", new TextNode(appleMusicPlaylist.getType()));
-			node.set("identifier", new TextNode(appleMusicPlaylist.getIdentifier()));
-			node.set("artworkUrl", new TextNode(appleMusicPlaylist.getArtworkURL()));
-			node.set("author", new TextNode(appleMusicPlaylist.getAuthor()));
-		} else if (playlist instanceof DeezerAudioPlaylist) {
-			var deezerPlaylist = (DeezerAudioPlaylist) playlist;
-			node.set("type", new TextNode(deezerPlaylist.getType()));
-			node.set("identifier", new TextNode(deezerPlaylist.getIdentifier()));
-			node.set("artworkUrl", new TextNode(deezerPlaylist.getArtworkURL()));
-			node.set("author", new TextNode(deezerPlaylist.getAuthor()));
-		} else if (playlist instanceof YandexMusicAudioPlaylist) {
-			var yandexMusicPlaylist = (YandexMusicAudioPlaylist) playlist;
-			node.set("type", new TextNode(yandexMusicPlaylist.getType()));
-			node.set("identifier", new TextNode(yandexMusicPlaylist.getIdentifier()));
-			node.set("artworkUrl", new TextNode(yandexMusicPlaylist.getArtworkURL()));
-			node.set("author", new TextNode(yandexMusicPlaylist.getAuthor()));
-		}
-	}
+    @Override
+    public JsonObject modifyAudioPlaylistPluginInfo(@NotNull AudioPlaylist playlist) {
+        if (playlist instanceof ExtendedAudioPlaylist) {
+            var extendedPlaylist = (ExtendedAudioPlaylist) playlist;
+
+            return new JsonObject(Map.of(
+            "type", JsonElementKt.JsonPrimitive(extendedPlaylist.getType()),
+            "identifier", JsonElementKt.JsonPrimitive(extendedPlaylist.getIdentifier()),
+            "artworkUrl", JsonElementKt.JsonPrimitive(extendedPlaylist.getArtworkURL()),
+            "author", JsonElementKt.JsonPrimitive(extendedPlaylist.getAuthor())
+            ));
+        }
+        return null;
+    }
 }
