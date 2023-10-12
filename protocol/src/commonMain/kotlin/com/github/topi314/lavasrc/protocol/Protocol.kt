b43d5d297a8712f15e41f6709dd83af28f40dd30
Toπ <git@topi.wtf>
2023-10-12 19:16:47 +0200

    add protocol module with types

---
+++ protocol/src/commonMain/kotlin/com/github/topi314/lavasrc/protocol/Protocol.kt
@@ -0,0 +1,52 @@
+package com.github.topi314.lavasrc.protocol
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ExtendedPlaylistInfo(
+    val type: Type,
+    val url: String,
+    val artworkUrl: String,
+    val author: String
+) {
+    /**
+     * The type of the originating track list.
+     */
+    @Serializable
+    public enum class Type {
+        /**
+         * A playlist from a music service.
+         */
+        @SerialName("playlist")
+        PLAYLIST,
+
+        /**
+         * An album listed on a music service.
+         */
+        @SerialName("album")
+        ALBUM,
+
+        /**
+         * An auto-generated playlist about an author from a music service.
+         */
+        @SerialName("artist")
+        ARTIST,
+
+        /**
+         * Recommendations from a music service (currently only Spotify).
+         */
+        @SerialName("recommendations")
+        RECOMMENDATIONS
+    }
+}
+
+@Serializable
+data class ExtendedAudioTrack(
+    val albumName: String?,
+    val albumUrl: String?,
+    val artistUrl: String?,
+    val artistArtworkUrl: String?,
+    val previewUrl: String?,
+    val isPreview: Boolean
+)
