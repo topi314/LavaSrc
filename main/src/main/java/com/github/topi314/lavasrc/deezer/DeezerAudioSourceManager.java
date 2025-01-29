5f503a8d1d557a0dbb8a6fca599578da173ad5dd
topi314 <git@topi.wtf>
2025-01-29 13:58:26 +0100

    make deezer readable default to false

--- main/src/main/java/com/github/topi314/lavasrc/deezer/DeezerAudioSourceManager.java
+++ main/src/main/java/com/github/topi314/lavasrc/deezer/DeezerAudioSourceManager.java
@@ -299,7 +299,7 @@ public class DeezerAudioSourceManager extends ExtendedAudioSourceManager impleme
 			if (!track.get("type").text().equals("track")) {
 				continue;
 			}
-			if (!track.get("readable").asBoolean(true)) {
+			if (!track.get("readable").asBoolean(false)) {
 				log.warn("Skipping track {} by {} because it is not readable. Available countries: {}", track.get("title").text(), track.get("artist").get("name").text(), track.get("available_countries").text());
 				continue;
 			}
@@ -309,7 +309,7 @@ public class DeezerAudioSourceManager extends ExtendedAudioSourceManager impleme
 	}
 
 	private AudioTrack parseTrack(JsonBrowser json, boolean preview) {
-		if (!json.get("readable").asBoolean(true)) {
+		if (!json.get("readable").asBoolean(false)) {
 			throw new FriendlyException("This track is not readable. Available countries: " + json.get("available_countries").text(),
 				FriendlyException.Severity.COMMON, null);
 		}
