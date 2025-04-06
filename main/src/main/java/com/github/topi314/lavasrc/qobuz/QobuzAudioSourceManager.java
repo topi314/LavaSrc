e330413e7d3b5ba239f6ea6a21b8064d6a2c4f37
Munish Khatri <109850452+munishkhatri720@users.noreply.github.com>
2025-04-07 04:45:15 +0530

    fixed missing import for ClosableHttpResponse

--- main/src/main/java/com/github/topi314/lavasrc/qobuz/QobuzAudioSourceManager.java
+++ main/src/main/java/com/github/topi314/lavasrc/qobuz/QobuzAudioSourceManager.java
@@ -19,6 +19,7 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import org.jetbrains.annotations.NotNull;
