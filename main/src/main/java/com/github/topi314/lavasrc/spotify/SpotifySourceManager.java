03b68d8c5c457d54c5d4608e9f9f5dec26bfa1b0
topi314 <git@topi.wtf>
2025-07-06 19:02:47 +0200

    fix spotify playlists still using anonymous tokens

--- main/src/main/java/com/github/topi314/lavasrc/spotify/SpotifySourceManager.java
+++ main/src/main/java/com/github/topi314/lavasrc/spotify/SpotifySourceManager.java
@@ -442,7 +442,7 @@ public class SpotifySourceManager extends MirroringAudioSourceManager implements
 		var offset = 0;
 		var pages = 0;
 		do {
-			page = this.getJson(API_BASE + "playlists/" + id + "/tracks?limit=" + PLAYLIST_MAX_PAGE_ITEMS + "&offset=" + offset, true, false);
+			page = this.getJson(API_BASE + "playlists/" + id + "/tracks?limit=" + PLAYLIST_MAX_PAGE_ITEMS + "&offset=" + offset, anonymous, this.preferAnonymousToken);
 			offset += PLAYLIST_MAX_PAGE_ITEMS;
 
 			for (var value : page.get("items").values()) {
