fa10754e21d17e0ef336cfbceb87c399be480f6d
asyncio <134554554+asynico@users.noreply.github.com>
2025-07-14 19:59:28 -0400

    Create LastfmConfig.java

---
+++ plugin/src/main/java/com/github/topi314/lavasrc/plugin/config/LastfmConfig.java
@@ -0,0 +1,20 @@
+package com.github.topi314.lavasrc.plugin.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@ConfigurationProperties(prefix = "plugins.lavasrc.lastfm")
+@Component
+public class LastfmConfig {
+
+    private String apiKey;
+
+    public String getApiKey() {
+        return this.apiKey;
+    }
+
+    public void setApiKey(String apiKey) {
+        this.apiKey = apiKey;
+    }
+
+}
