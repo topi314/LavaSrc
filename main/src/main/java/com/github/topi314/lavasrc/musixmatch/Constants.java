57a6625667862b0a10815c4c4e620d7f831403fb
bob <134554554+asynico@users.noreply.github.com>
2025-08-08 18:43:32 -0400

    Update Constants.java

--- main/src/main/java/com/github/topi314/lavasrc/musixmatch/Constants.java
+++ main/src/main/java/com/github/topi314/lavasrc/musixmatch/Constants.java
@@ -1,16 +1,26 @@
 package com.github.topi314.lavasrc.musixmatch;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
-public class Constants {
-    public static final Map<String, String> ENDPOINTS = Map.of(
-        "TOKEN", "https://apic-desktop.musixmatch.com/ws/1.1/token.get?app_id=web-desktop-app-v1.0",
-        "SEARCH", "https://apic-desktop.musixmatch.com/ws/1.1/track.search?app_id=web-desktop-app-v1.0&page_size=3&page=1&s_track_rating=desc",
-        "LYRICS", "https://apic-desktop.musixmatch.com/ws/1.1/track.subtitle.get?app_id=web-desktop-app-v1.0&subtitle_format=lrc",
-        "ALT_LYRICS", "https://apic-desktop.musixmatch.com/ws/1.1/macro.subtitles.get?format=json&namespace=lyrics_richsynched&subtitle_format=mxm&app_id=web-desktop-app-v1.0"
-    );
+public final class Constants {
+    private Constants() {}
 
-    public static final String TIMESTAMPS_REGEX = "\\[\\d+:\\d+\\.\\d+\\]";
-    public static final String EMPTY_LINES_REGEX = "^\\s*$";
-    public static final String ARTIST_TITLE_REGEX = "^(.*?)\\s*[-–~]\\s*(.+)$";
+    public static final Map<String, String> ENDPOINTS;
+    static {
+        Map<String, String> endpoints = new HashMap<>();
+        endpoints.put("TOKEN", "https://apic-desktop.musixmatch.com/ws/1.1/token.get?app_id=web-desktop-app-v1.0");
+        endpoints.put("SEARCH", "https://apic-desktop.musixmatch.com/ws/1.1/track.search?app_id=web-desktop-app-v1.0&page_size=3&page=1&s_track_rating=desc");
+        endpoints.put("LYRICS", "https://apic-desktop.musixmatch.com/ws/1.1/track.subtitle.get?app_id=web-desktop-app-v1.0&subtitle_format=lrc");
+        endpoints.put("ALT_LYRICS", "https://apic-desktop.musixmatch.com/ws/1.1/macro.subtitles.get?format=json&namespace=lyrics_richsynched&subtitle_format=mxm&app_id=web-desktop-app-v1.0");
+        ENDPOINTS = Collections.unmodifiableMap(endpoints);
+    }
+
+    public static final class Regex {
+        private Regex() {}
+        public static final String TIMESTAMPS = "\\[\\d+:\\d+\\.\\d+\\]";
+        public static final String EMPTY_LINES = "^\\s*$";
+        public static final String ARTIST_TITLE = "^(.*?)\\s*[-–~]\\s*(.+)$";
+    }
 }
